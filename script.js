42e3e1186a61758c883d8bc6a0e7955bc562bd04
A0557073007 <ch8vt8gbgv@privaterelay.appleid.com>
2026-09-16 06:44:36 +0300

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const b=document.getElementById('menuBtn'),n=document.getElementById('nav');b.onclick=()=>n.classList.toggle('open');document.querySelectorAll('#nav a').forEach(a=>a.onclick=()=>n.classList.remove('open'));
