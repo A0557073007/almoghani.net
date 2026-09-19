843042203de95e950d5e14b9cd6348e98f054e82
A0557073007 <ch8vt8gbgv@privaterelay.appleid.com>
2026-09-19 18:41:05 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='almoghani-v1';
+const ASSETS=['./','./index.html','./design.jpg','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
